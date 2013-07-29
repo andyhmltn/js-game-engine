ac42d13512c8df2f248c54697e1fa0e194709607
Andy Hamilton <andyhmltn@gmail.com>
2013-07-29 14:24:41 +0100

    Seperated the engine code from the example

--- js/vendor/engine.js
+++ js/vendor/engine.js
@@ -74,15 +74,3 @@ GameEngineObject.prototype.update_position = function() {
 
   this.draw();
 }
- 
-var canvas = document.getElementById('the-canvas');
-var scene = new GameEngineScene(500,500, canvas);
-
-scene.add_object({
-  template: 'ball',
-  radius  : 10,
-  x       : 100,
-  y       : 100
-});
-
-scene.run();
